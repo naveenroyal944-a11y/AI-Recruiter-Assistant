1765f8404bcf26d31b8ce1c9f04aa0598b7ad85c
Penugonda Naveen <naveenroyal944@gmail.com>
2026-06-07 08:29:50 +0530

    correct import

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PyPDF2 import PdfReader
+import pandas as pd
 
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
